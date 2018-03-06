ccc37e3c960652bcf7dbe9b4738ef3a47828d1af
Gonzalo Kurin <gonzalokurin@Gonzalos-MacBook-Pro.local>
2018-03-06 17:19:09 -0300

    Se ven preguntas en la publicacion

--- publicacion.php
+++ publicacion.php
@@ -9,6 +9,12 @@ include_once("configuracion.php");
 
 $conn->conectar();
 
+if (!isset($_SESSION["user"])) {
+    $usuario = false;
+} else {
+    $usuario = $_SESSION["user"];
+}
+
 $param = array(
     array("id", $_GET["publicacion"], "int")
 );
@@ -38,11 +44,31 @@ $conn->consulta($sql2, $param2);
 
 $especie = $conn->siguienteRegistro();
 
+$param3 = array(
+    array("id", $_GET["publicacion"], "int")
+);
+
+$sql3 = "select * from preguntas where id_publicacion = :id";
+$conn->consulta($sql3, $param3);
+
+
+$preguntas = $conn->restantesRegistros();
+
+while (list($clave, $valor) = each($preguntas)) {
+    if ($valor['respuesta'] == null) {
+        $preguntas[$clave]['respuesta'] = "No hay respuesta aun.";
+    }
+}
+
+reset($preguntas);
+
 
 
 
 $smarty->assign("publicacion", $publicacion);
 $smarty->assign("especie", $especie);
+$smarty->assign("preguntas", $preguntas);
+$smarty->assign("usuario", $usuario);
 
 /*
  * ENVIO EL TEMPLATE AL CLIENTE
