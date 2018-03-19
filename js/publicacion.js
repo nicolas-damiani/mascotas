7b84538d60004c7debd89505c90b53a870a51c56
Nicolas Damiani <nico_damiani@hotmail.com>
2018-03-19 18:53:30 -0300

    no message

--- js/publicacion.js
+++ js/publicacion.js
@@ -4,29 +4,29 @@ $(document).ready(function () {
             $('#contenedorNuevaPregunta').css('display', 'block');
         }
     });
-    
+
     $('#cerrarPublicacion').on({
         'click': function () {
             cerrarPublicacion();
         }
     });
-    
+
     $('#exportar').on({
         'click': function () {
             exportarPublicacion();
         }
     });
-    
+
 
     $('#imagenesPublicacion').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                autoplay: true,
-                autoplaySpeed: 2600,
-                dots: true,
-             //   appendDots: $('#circle')
-          //      prevArrow: '<img class="slick-prev" src="/files/layouts/leftArrowColor.png" style="display: block;height: 38px;position: absolute;top: 41%;margin-bottom: 0px;margin-left: 18px;z-index: 2;" />',
-           //     nextArrow: '<img class="slick-next" src="/files/layouts/rightArrowColor.png" style="display: block;height: 38px;position: absolute;float: right;margin-right: 21px;z-index: 2;right: 0;top: 41%;"/>'
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2600,
+        dots: true,
+        //   appendDots: $('#circle')
+        //      prevArrow: '<img class="slick-prev" src="/files/layouts/leftArrowColor.png" style="display: block;height: 38px;position: absolute;top: 41%;margin-bottom: 0px;margin-left: 18px;z-index: 2;" />',
+        //     nextArrow: '<img class="slick-next" src="/files/layouts/rightArrowColor.png" style="display: block;height: 38px;position: absolute;float: right;margin-right: 21px;z-index: 2;right: 0;top: 41%;"/>'
     });
 
     $('#realizarPregunta').on({
@@ -40,9 +40,37 @@ $(document).ready(function () {
             }
         }
     });
+
+
 });
 
-function exportarPublicacion(){
+
+function initMap() {
+    if ($('#latitud').val() != "" && $('#longitud').val() != "") {
+
+        var lat = parseInt($('#latitud').val());
+        var long = parseInt($('#longitud').val());
+
+
+        var uluru = {lat: lat, lng: long};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    } else {
+        $('#map').css('display', 'none');
+    }
+}
+
+
+
+
+
+function exportarPublicacion() {
     $.ajax({
         url: "publicacion.php",
         dataType: "json",
@@ -55,20 +83,20 @@ function exportarPublicacion(){
     }).done(function (data) {
         if (data.status == "ok") {
             alert('publicacion cerrada');
-        }else{
+        } else {
             alert("error cerrando publicacion");
         }
     })
 }
 
-function cerrarPublicacion(){
+function cerrarPublicacion() {
     var exitoso = $('#selectExito').val();
     var idPublicacion = $('#idPublicacion').html();
     $.ajax({
         url: "publicacion.php",
         dataType: "json",
         type: "POST",
-        data: "accion=cerrarPublicacion&exitosa=" + exitoso+"&idPublicacion="+idPublicacion,
+        data: "accion=cerrarPublicacion&exitosa=" + exitoso + "&idPublicacion=" + idPublicacion,
         timeout: 2000,
         beforeSend: function () {
             //cargando();
@@ -76,7 +104,7 @@ function cerrarPublicacion(){
     }).done(function (data) {
         if (data.status == "ok") {
             alert('publicacion cerrada');
-        }else{
+        } else {
             alert("error cerrando publicacion");
         }
     })
